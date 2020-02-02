5472928deef57e66a88222fd0d60b132317fd013
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2020-02-01 22:41:32 -0500

    Added basic movement functionality

---
+++ Zelda-like-game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start(){
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    // To move character from other places (on screen buttons)
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            // Get player position and add change (1) multiply it by speed (4)
+            // and multiply by how much time has past since previous frame (small amount)
+            // Therefore, we move a small amount each frame
+           transform.position + change * speed * Time.deltaTime
+            );
+    }
+}
