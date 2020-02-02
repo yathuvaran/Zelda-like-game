575213708f414d374d417c15ecdc842706950e0c
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2020-02-01 23:33:26 -0500

    Created Animations for movements and blend trees

--- Zelda-like-game/Assets/Scripts/PlayerMovement.cs
+++ Zelda-like-game/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start(){
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -18,9 +20,21 @@ public class PlayerMovement : MonoBehaviour {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
 
