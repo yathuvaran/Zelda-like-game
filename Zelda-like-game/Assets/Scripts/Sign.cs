7ed4390de04c6b5cb37c78f6407f82b3ac042af4
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-05-24 18:13:24 -0400

    added signs and dialog

---
+++ Zelda-like-game/Assets/Scripts/Sign.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox; //reference to dialog box itself
+    public Text dialogText; //reference the text
+    public string dialog; //reference the string to show up in place of dialog
+    public bool playerInRange; //boolean to determine if dialog box should be active
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
