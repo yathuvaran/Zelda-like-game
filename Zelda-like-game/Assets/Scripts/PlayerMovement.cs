82a7bfd66661e7207c5da0f9a306d10227771a84
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-05-24 22:59:53 -0400

    Added AttackAnimations and PlayerMovement State Machine

--- Zelda-like-game/Assets/Scripts/PlayerMovement.cs
+++ Zelda-like-game/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour {
 
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -11,8 +19,11 @@ public class PlayerMovement : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start(){
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -20,7 +31,25 @@ public class PlayerMovement : MonoBehaviour {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null; //wait one frame since we can return to attacking state from attacking state
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+
     }
 
     void UpdateAnimationAndMove()
@@ -41,6 +70,7 @@ public class PlayerMovement : MonoBehaviour {
     // To move character from other places (on screen buttons)
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
             // Get player position and add change (1) multiply it by speed (4)
             // and multiply by how much time has past since previous frame (small amount)
