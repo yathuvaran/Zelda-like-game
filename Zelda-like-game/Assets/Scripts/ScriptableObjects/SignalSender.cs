983572d4e53321c07e00173d0c4e14bb8e617b84
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-06-05 14:47:57 -0400

    Added Scriptable Objects to make a Signal System

---
+++ Zelda-like-game/Assets/Scripts/ScriptableObjects/SignalSender.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SignalSender : ScriptableObject
+{
+    public List<SignalListener> listeners = new List<SignalListener>();
+
+    public void Raise()
+    {
+        //go through list of listeners backwards in case listener removes itself from list and causes out of range exception
+        for(int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnSignalRaised();
+        }
+    }
+
+    public void RegisterListener(SignalListener listener)
+    {
+        listeners.Add(listener);
+    }
+
+    public void DeRegisterListener(SignalListener listener)
+    {
+        listeners.Remove(listener);
+    }
+}
