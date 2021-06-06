7fe73cc0b03a71347fbacc263159b17727f41bd6
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-06-05 21:06:41 -0400

    Added initial heart system UI feature

--- Zelda-like-game/Assets/Scripts/PlayerMovement.cs
+++ Zelda-like-game/Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,8 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    public FloatValue currentHealth;
+    public SignalSender playerHealthSignal;
 
     // Start is called before the first frame update
     void Start(){
@@ -82,9 +84,14 @@ public class PlayerMovement : MonoBehaviour {
             );
     }
 
-    public void Knock(float knockTime)
+    public void Knock(float knockTime, float damage)
     {
-        StartCoroutine(KnockCo(knockTime));
+        currentHealth.initialValue -= damage;
+        if (currentHealth.initialValue > 0)
+        {
+            playerHealthSignal.Raise();
+            StartCoroutine(KnockCo(knockTime));
+        }
     }
 
     private IEnumerator KnockCo(float knockTime)
