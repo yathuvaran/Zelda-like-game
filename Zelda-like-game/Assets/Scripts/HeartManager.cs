392bbcf22864fe0e8dde5395effbd120f124da3a
yathuvaran <49103000+yathuvaran@users.noreply.github.com>
2021-06-07 21:24:37 -0400

    Added support for hearts decreasing when taking damage

--- Zelda-like-game/Assets/Scripts/HeartManager.cs
+++ Zelda-like-game/Assets/Scripts/HeartManager.cs
@@ -11,6 +11,7 @@ public class HeartManager : MonoBehaviour
     public Sprite halfFullHeart;
     public Sprite emptyHeart;
     public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +24,32 @@ public class HeartManager : MonoBehaviour
         {
             hearts[i].gameObject.SetActive(true);
             hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        //consider half heart as one health point (5 health points = 5/2 = 2.5 hearts)
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            //indexing of hearts since if we have 3 health points (1.5 hearts)
+            //0 and 1 can be considered full heart (so we tempHealth - 1)
+            if (i <= tempHealth - 1)
+            {
+                //Full Heart
+                hearts[i].sprite = fullHeart;
+            }
+            else if (i >= tempHealth)
+            {
+                //Empty Heart
+                hearts[i].sprite = emptyHeart;
+            }
+            else
+            {
+                //Half Full heart
+                hearts[i].sprite = halfFullHeart;
+            }
         }
     }
 
