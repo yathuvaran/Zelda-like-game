38d81a53c3a2632281e90d0a06172bc704fcd49b
Anandarajah Yathuvaran <yathuvaran@gmail.com>
2021-06-20 15:36:02 -0400

    added scene transitions

--- Zelda-like-game/Assets/Scripts/PlayerMovement.cs
+++ Zelda-like-game/Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour {
     private Animator animator;
     public FloatValue currentHealth;
     public SignalSender playerHealthSignal;
+    public VectorValue startingPosition;
 
     // Start is called before the first frame update
     void Start(){
@@ -28,6 +29,7 @@ public class PlayerMovement : MonoBehaviour {
         myRigidbody = GetComponent<Rigidbody2D>();
         animator.SetFloat("moveX", 0);
         animator.SetFloat("moveY", -1);
+        transform.position = startingPosition.initialValue;
     }
 
     // Update is called once per frame
